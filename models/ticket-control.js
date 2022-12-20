bf734182e1225ac56d2a073c22337fd5d796187a
LeoardoCG <andresxach7@gmail.com>
2022-12-19 22:21:21 -0600

    controller, db

---
+++ models/ticket-control.js
@@ -0,0 +1,11 @@
+
+
+class TicketContro {
+
+    constructor() {
+        this.ultimo =  0;
+        this.hoy    = new Date().getDate();
+        this.tickets = [];
+        this.ultimos4 = [];
+    }
+}
